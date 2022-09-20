92fb5acb92b24d89415928e9a7b5359b085ebeb2
Mariana Machohan <debrisofme9@gmail.com>
2022-09-20 23:58:28 +0200

    1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 
 import './App.css';
+import Test1 from './Test1';
 
 export default function App() {
 
 
-	
+
   return (
     <>
-
+			<Test1/>
+			{/* console.log(this.state) */}
     
     </>
   );
