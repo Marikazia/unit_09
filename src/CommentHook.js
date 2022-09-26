d0e21611717c02e3f2ab4a8d203a01b22ad5caff
Mariana Machohan <debrisofme9@gmail.com>
2022-09-27 01:00:42 +0200

    1

--- src/CommentHook.js
+++ src/CommentHook.js
@@ -1,38 +1,71 @@
 import { Component } from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 
-class CommentHook extends Component {
-	constructor() {
-		super();
-		this.state = { data: [] };
-	}
+// class CommentHook extends Component {
+// 	constructor() {
+// 		super();
+// 		this.state = { data: [] };
+// 	}
 
-	selectHandler = (event) => {
-		console.log(event.target.value);
-		fetch("https://jsonplaceholder.typicode.com/posts/"+ event.target.value +"/comments")
-			.then(response => response.json())
-			.then(data => {
-				console.log(data);
-				this.setState({ data });
-		});
+// 	selectHandler = (event) => {
+// 		console.log(event.target.value);
+// 		fetch("https://jsonplaceholder.typicode.com/posts/"+ event.target.value +"/comments")
+// 			.then(response => response.json())
+// 			.then(data => {
+// 				console.log(data);
+// 				this.setState({ data });
+// 		});
 
-	}
+// 	}
 
-	render() {
-		return (
-			<>
-				<select onChange={this.selectHandler}>
+// 	render() {
+// 		return (
+// 			<>
+// 				<select onChange={this.selectHandler}>
 
-					<option value="1">1</option>
-					<option value="2">2</option>
-					<option value="3">3</option>
-					<option value="4">4</option>
+// 					<option value="1">1</option>
+// 					<option value="2">2</option>
+// 					<option value="3">3</option>
+// 					<option value="4">4</option>
 
-				</select>
+// 				</select>
 			
-			</>
-		)
-	}
+// 			</>
+// 		)
+// 	}
+// }
 
+function CommentHook() {
+	const [data, setData] = useState([]);
+
+	useEffect((event) => {
+		fetch("https://jsonplaceholder.typicode.com/posts/"+ event.target.value +"/comments")
+		.then(response => response.json())
+		.then(data => {
+			console.log(data);
+			// this.setState({ data });
+			setData( data );
+		});
+	});
+
+
+
+	return (
+
+		<>
+			<select onChange={this.selectHandler}>
+
+				<option value="1">1</option>
+				<option value="2">2</option>
+				<option value="3">3</option>
+				<option value="4">4</option>
+
+			</select>
+		
+		</>
+
+	)
 
 }
 
