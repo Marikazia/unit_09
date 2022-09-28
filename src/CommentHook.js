8f59fc398e9c1f7dcc7cb28013ac2a3148be9179
Mariana Machohan <debrisofme9@gmail.com>
2022-09-28 02:55:34 +0200

    1

--- src/CommentHook.js
+++ src/CommentHook.js
@@ -1,70 +1,29 @@
-import { Component } from "react";
-import { useState } from "react";
-import { useEffect } from "react";
-
-// class CommentHook extends Component {
-// 	constructor() {
-// 		super();
-// 		this.state = { data: [] };
-// 	}
-
-// 	selectHandler = (event) => {
-// 		console.log(event.target.value);
-// 		fetch("https://jsonplaceholder.typicode.com/posts/"+ event.target.value +"/comments")
-// 			.then(response => response.json())
-// 			.then(data => {
-// 				console.log(data);
-// 				this.setState({ data });
-// 		});
-
-// 	}
-
-// 	render() {
-// 		return (
-// 			<>
-// 				<select onChange={this.selectHandler}>
-
-// 					<option value="1">1</option>
-// 					<option value="2">2</option>
-// 					<option value="3">3</option>
-// 					<option value="4">4</option>
-
-// 				</select>
-			
-// 			</>
-// 		)
-// 	}
-// }
+import React, { useState, useEffect, Component } from "react";
 
 function CommentHook() {
 	const [data, setData] = useState([]);
 
 	useEffect((event) => {
 		fetch("https://jsonplaceholder.typicode.com/posts/"+ event.target.value +"/comments")
-		.then(response => response.json())
-		.then(data => {
-			console.log(data);
-			// this.setState({ data });
-			setData( data );
-		});
-	});
-
+			.then(response => response.json())
+			.then(data => {
+				console.log(data);
+				// this.setState({ data });
+				setData( data );
+			});
+	}, []);
 
 
 	return (
-
 		<>
-			<select onChange={this.selectHandler}>
-
+			<select onChange={this.event}>
 				<option value="1">1</option>
 				<option value="2">2</option>
 				<option value="3">3</option>
 				<option value="4">4</option>
-
 			</select>
-		
+	
 		</>
-
 	)
 
 }
