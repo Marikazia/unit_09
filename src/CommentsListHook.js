7e8bc504eecbb06351c239af25565e8d94899b17
Mariana Machohan <debrisofme9@gmail.com>
2022-10-05 09:11:03 +0200

    1

--- src/CommentsListHook.js
+++ src/CommentsListHook.js
@@ -1,18 +1,43 @@
 import React, { useState, useEffect, Component } from "react";
-// import 
+// import Comment2Hook from "./Comment2Hook";
 
 export default function CommentsListHook() {
+	// const [state, setState] = useState([]);
+
+
+	// function onlyEven(event) {
+	// 	console.log(event.target.value);
+	// 	fetch("https://jsonplaceholder.typicode.com/posts/"+ event.target.value +"/comments")
+	// 		.then(response => response.json())
+	// 		.then(state => {
+	// 			console.log(state);
+	// 			setState( state );
+	// 	});
+	// }
+	const [state, setState] = useState([]);
+
+	function selectHandler(event) {
+		console.log(event.target.value);
+		fetch("https://jsonplaceholder.typicode.com/posts/"+ event.target.value +"/comments")
+			.then(response => response.json())
+			.then(state => {
+				console.log(state);
+				setState( state );
+		});
+	}
+
 	return (
 		<div>
+			<p>Choose post ID:</p>
+			<select onChange={this.selectHandler}>
+				<option value="1">1</option>
+				<option value="2">2</option>
+				<option value="3">3</option>
+				<option value="4">4</option>
+			</select>
 			<div>
-				<button onClick={onlyEven}>Only even comments</button>
+			<CommentsListHook data={this.state.data}/>
 			</div>
-			{props.map((el, index) => (
-				<section key={el.id}>
-					<p><b>{index + 1}. {el.email}</b></p>
-					<p>{el.body}</p>
-				</section>
-			))}
 		</div>
 	)
 }
