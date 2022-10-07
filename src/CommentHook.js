7e2466fbab4563d02658618ecb87756833e99b25
Mariana Machohan <debrisofme9@gmail.com>
2022-10-07 03:14:06 +0200

    1

--- src/CommentHook.js
+++ src/CommentHook.js
@@ -1,17 +1,17 @@
 import React, { useState, useEffect, Component } from "react";
 
 export default function CommentHook() {
-	// const [state, setState] = useState([]); //data!!!!
+	const [state, setState] = useState([]); //data!!!!
 
-	// function selectHandler(event) {
-	// 	console.log(event.target.value);
-	// 	fetch("https://jsonplaceholder.typicode.com/posts/"+ event.target.value +"/comments")
-	// 		.then(response => response.json())
-	// 		.then(state => {
-	// 			console.log(state);
-	// 			setState( state );
-	// 	});
-	// }
+	function selectHandler(event) {
+		console.log(event.target.value);
+		fetch("https://jsonplaceholder.typicode.com/posts/"+ event.target.value +"/comments")
+			.then(response => response.json())
+			.then(state => {
+				console.log(state);
+				setState( state );
+		});
+	}
 
 	return (
 		<>
